1a4e5b03f381087f4a3b209d4f1e4fb9780a397b
Booyoun <booyoun@creder.biz>
2023-08-25 18:33:56 +0900

    feat: add buy, sell function

--- contracts/Itemswap.sol
+++ contracts/Itemswap.sol
@@ -9,8 +9,9 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Itemswap is Ownable {
     uint8 public cw = 10;
 
-    // 0.03935: decimal 18
-    uint256 public roseUSDPrice = 39350000000000000;
+    // 0.03935 -> 3.935 100배 높여서 계산: decimal 18
+    // uint256 public roseUSDPrice = 39350000000000000;
+    uint256 public roseUSDPrice = 3935000000000000000;
     // 0.62: decimal 18
     uint256 public gpcUSDPrice = 620000000000000000;
 
@@ -34,54 +35,93 @@ contract Itemswap is Ownable {
 
     constructor() payable {}
 
-    function getItemUSDPrice() public view returns (uint256) {
+    function getItemUSDPrice(uint8 _sellFlag) public view returns (uint256) {
+        require(_sellFlag == 1 || _sellFlag == 0, "_sellFlag should be 0 or 1");
+
         // cw, gpcBalance, itemBalance
-        uint256 reserve = itemBalance / cw;
-        uint256 itemGPCPrice = gpcBalance / reserve;
+        uint256 reserve = 10**18 * itemBalance / cw;
+        uint256 itemGPCPrice = 10**18 * gpcBalance / reserve;
+
+        if (_sellFlag == 1) {
+            uint256 estimatedItemBalance = itemBalance - 1;
+            uint256 estimatedGPCBalance = gpcBalance - itemGPCPrice;
+
+            reserve = 10**18 * estimatedItemBalance / cw;
+            itemGPCPrice = 10**18 * estimatedGPCBalance / reserve;
+        }
+
         // // 5000000000000000000
         // console.log("itemGPCPrice: ", itemGPCPrice);
         uint256 itemUSDPrice = itemGPCPrice * gpcUSDPrice / 1000000000000000000;
         return itemUSDPrice;
     }
 
-    // function getItemROSEPrice() public view returns (uint256) {
-
-
-    //     uint256 itemGPCPrice = gpcBalance / itemBalance / cw;
-    //     uint256 itemUSDPrice = itemGPCPrice * gpcUSDPrice / 1000000;
-
-    //     roseUSDPrice / itemUSDPrice;
-
-
-    //     return itemUSDPrice;
-
-    // }
+    function getItemROSEPrice(uint8 _sellFlag) public view returns (uint256) {
+        uint256 itemUSDPrice = getItemUSDPrice(_sellFlag);
+        uint256 itemValueBaseRose = 10**18 * itemUSDPrice / roseUSDPrice;
+        return itemValueBaseRose;
+    }
 
     function buy(uint256 _amount) payable public nonReentrant {
         // 우선 1개씩만 테스트
+        require(_amount == 1, "[TEST] _amount should be 1");
+
         require(itemBalance >= _amount, "Insufficient item");
 
         // msg.value 확인
 
         // 아이템 개당 가격
-        uint256 itemUSDPrice = getItemUSDPrice();
+        uint256 itemROSEPrice = getItemROSEPrice(0);
 
-        require(itemUSDPrice * _amount == msg.value, "Invalid ROSE Amount");
+        require(itemROSEPrice * _amount == msg.value, "Invalid ROSE Amount");
 
         // TODO: msg.value -> GPC 로 바뀌고... 컨트랙트 gpcBalance 증가
-
-
-
-
-
-
-        // TODO: amount 감소 -> 유저 item amount 증가
         
-        
+        // ROSE -> GPC
+        uint256 itemUSDPrice = msg.value * roseUSDPrice / 10**18;
+        console.log("[TEST] itemUSDPrice ", itemUSDPrice);
+
+        uint256 itemGPCPrice = 10**18 * itemUSDPrice / gpcUSDPrice;
+        console.log("[TEST] itemGPCPrice: ", itemGPCPrice);
+        // 4999999999999999998
+
+        // 컨트랙트 GPC 보증금 증가
+        gpcBalance += itemGPCPrice;
+
+        // TODO: 컨트랙트 amount 감소 -> 유저 item amount 증가
+        itemBalance -= _amount;
+        userItemAmount[msg.sender] += _amount;
     }
 
-    function sell() public nonReentrant {
+    function sell(uint256 _amount) public nonReentrant {
+        // 우선 1개씩만 테스트
+        require(_amount == 1, "[TEST] _amount should be 1");
 
+        // 유저한테 _amount 가 있나?
+        require(userItemAmount[msg.sender] >= _amount, "Insufficient users's _amount");
+
+        // 팔 때는 (전체 아이템 개수 - 1)로 계산해서 팔아야함
+        // 유저에게 지급할 ROSE: 아이템 개당 가격
+        uint256 itemROSEValue = getItemROSEPrice(1);
+
+        // TODO:
+        // require(itemROSEPrice * _amount <= address(this)의 balance, "Invalid ROSE Amount");
+
+        
+        // ROSE -> GPC
+        uint256 itemUSDPrice = itemROSEValue * roseUSDPrice / 10**18;
+        console.log("[TEST] itemUSDPrice ", itemUSDPrice);
+
+        uint256 itemGPCPrice = 10**18 * itemUSDPrice / gpcUSDPrice;
+        console.log("[TEST] itemGPCPrice: ", itemGPCPrice);
+        // 4999999999999999998
+
+        // 컨트랙트의 GPC 보증금 감소
+        gpcBalance -= itemGPCPrice;
+
+        // TODO: 컨트랙트 amount 증가 -> 유저 item amount 감소
+        itemBalance += _amount;
+        userItemAmount[msg.sender] -= _amount;
     }
 
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,26 +11,94 @@ async function main() {
   // await tGPC.waitForDeployment();
   // console.log("deployed GPC");
 
-  const roseAmount = hre.ethers.parseEther("2.5");
+  const [owner, otherAccount] = await ethers.getSigners();
+  console.log("owner: ", owner.address);
+
+  const roseAmount = hre.ethers.parseEther("1");
   const itemswap = await hre.ethers.deployContract("Itemswap", {
     value: roseAmount,
   });
   await itemswap.waitForDeployment();
   console.log("deployed itemswap");
+
   
-  let itemUSDPrice = await itemswap.getItemUSDPrice();
-  console.log("itemUSDPrice: ", itemUSDPrice);
+  
+  let itemUSDPriceWei = await itemswap.getItemUSDPrice(0);
+  console.log("itemUSDPriceWei: ", itemUSDPriceWei);
   // 3.1 USD
+  let itemUSDPrice = hre.ethers.formatEther(itemUSDPriceWei);
+  console.log("itemUSDPrice: ", itemUSDPrice);
+
 
   // item 당 ROSE 환산 가격
   let roseUSDPrice = await itemswap.roseUSDPrice();
   console.log("1 ROSE = ", roseUSDPrice);
 
-  let itemPriceBaseRose = parseFloat(itemUSDPrice) / parseFloat(roseUSDPrice);
-  console.log("1개 item의 ROSE 환산가: ", itemPriceBaseRose + " ROSE");
-  
-  
+  // let itemPriceBaseRose = parseFloat(itemUSDPrice) / parseFloat(roseUSDPrice);
+  // console.log("1개 item의 ROSE 환산가: ", itemPriceBaseRose + " ROSE");
 
+  let itemPriceBaseRose = await itemswap.getItemROSEPrice(0);
+  console.log("itemPriceBaseRose: ", itemPriceBaseRose);
+
+  // TVL 계산
+  // item 전체 개수, GPC 보증금 합
+  let itemBalance = await itemswap.itemBalance();
+  console.log("itemBalance: ", itemBalance);
+
+  // 78.780177890724269377
+  let gpcBalanceWei = await itemswap.gpcBalance();
+  console.log("gpcBalanceWei: ", gpcBalanceWei);
+  let gpcBalance = hre.ethers.formatEther(gpcBalanceWei);
+  console.log("gpcBalance: ", gpcBalance);
+
+  let gpcUSDPriceWei = await itemswap.gpcUSDPrice();
+  console.log("gpcUSDPriceWei: ", gpcUSDPriceWei);
+  let gpcUSDPrice = hre.ethers.formatEther(gpcUSDPriceWei);
+  console.log("gpcUSDPrice: ", gpcUSDPrice);
+
+  let tvl = parseFloat(itemBalance) * parseFloat(itemUSDPrice) + parseFloat(gpcBalance) * parseFloat(gpcUSDPrice);
+  console.log("tvl: ", tvl + " USD");
+
+  // item 구매 전 유저의 item 개수
+  let userItemAmount = await itemswap.userItemAmount(owner.address);
+  console.log("userItemAmount: ", userItemAmount);
+
+  // item 구매
+  await itemswap.buy(1, {
+    value: itemPriceBaseRose
+  });
+
+  // item 구매 후 유저의 item 개수
+  let userItemAmount2 = await itemswap.userItemAmount(owner.address);
+  console.log("userItemAmount2: ", userItemAmount2);
+
+  // item 1개 구매 후 item 가격 계산
+  let itemPriceBaseRose2 = await itemswap.getItemROSEPrice(0);
+  console.log("itemPriceBaseRose2: ", itemPriceBaseRose2);
+
+
+  // 판매를 위한 item ROSE 가격 계산
+  let itemPriceBaseRose3 = await itemswap.getItemROSEPrice(1);
+  console.log("itemPriceBaseRose3: ", itemPriceBaseRose3);
+
+  console.log("-------------------- 판매 테스트 -------------------- ");
+
+  // item 판매
+  await itemswap.sell(1);
+
+  // item 판매 후 유저의 item 개수
+  let userItemAmount3 = await itemswap.userItemAmount(owner.address);
+  console.log("userItemAmount3: ", userItemAmount3);
+
+  // item 판매후 아이템 구매가
+  let itemPriceBaseRose4 = await itemswap.getItemROSEPrice(0);
+  console.log("item 판매후 아이템 구매가: ", itemPriceBaseRose4);
+
+  // item 판매후 아이템 판매가
+  let itemPriceBaseRose5 = await itemswap.getItemROSEPrice(1);
+  console.log("item 판매후 아이템 판매가: ", itemPriceBaseRose5);
+
+  // item 판매후 유저 ROSE 
 
 
   
